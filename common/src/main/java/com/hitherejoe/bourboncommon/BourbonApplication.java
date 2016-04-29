420bc8bd231f0704cf9e981e8847b7c4df7bda47
Joe Birch <hitherejoebirch@googlemail.com>
2016-04-29 19:55:58 +0100

    Move application class to common module

---
+++ common/src/main/java/com/hitherejoe/bourboncommon/BourbonApplication.java
@@ -0,0 +1,40 @@
+package com.hitherejoe.bourboncommon;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.hitherejoe.bourboncommon.BuildConfig;
+import com.hitherejoe.bourboncommon.injection.component.ApplicationComponent;
+import com.hitherejoe.bourboncommon.injection.component.DaggerApplicationComponent;
+import com.hitherejoe.bourboncommon.injection.module.ApplicationModule;
+
+import timber.log.Timber;
+
+public class BourbonApplication extends Application  {
+
+    ApplicationComponent mApplicationComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) Timber.plant(new Timber.DebugTree());
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public static BourbonApplication get(Context context) {
+        return (BourbonApplication) context.getApplicationContext();
+    }
+
+    public ApplicationComponent getComponent() {
+        return mApplicationComponent;
+    }
+
+    // Needed to replace the component with a test specific one
+    public void setComponent(ApplicationComponent applicationComponent) {
+        mApplicationComponent = applicationComponent;
+    }
+}
+
