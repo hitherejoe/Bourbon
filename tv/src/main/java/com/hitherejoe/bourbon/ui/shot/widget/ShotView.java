7144438917b89edca2ea6ab7d71c871b8356d437
Joe Birch <hitherejoebirch@googlemail.com>
2016-05-13 20:41:20 +0100

    Fix tv detail bar animation

--- tv/src/main/java/com/hitherejoe/bourbon/ui/shot/widget/ShotView.java
+++ tv/src/main/java/com/hitherejoe/bourbon/ui/shot/widget/ShotView.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.hitherejoe.bourbon.R;
 import com.hitherejoe.bourboncorecommon.data.model.Shot;
+import com.hitherejoe.bourboncorecommon.util.DisplayMetricsUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -75,20 +76,33 @@ public class ShotView extends FrameLayout {
             @Override
             public void run() {
                 animateViewOut();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        animateViewIn();
+                    }
+                }, 2000);
             }
         }, 2000);
     }
 
     private void animateViewIn() {
-        mShotDetail.animate().translationY(mShotDetail.getTop() + mShotDetail.getHeight())
+        mShotDetail.animate().translationY(0)
                 .setInterpolator(new LinearOutSlowInInterpolator())
-                .setListener(mAnimatorListener).start();
+                .setDuration(700)
+                .setListener(mAnimatorListener)
+                .start();
     }
 
     private void animateViewOut() {
-       mShotDetail.animate().translationY(mShotDetail.getTop() - mShotDetail.getHeight())
+        float margin = getResources().getDimension(R.dimen.shot_detail_bar_margin_bottom);
+        int translationValue = mShotDetail.getHeight() + DisplayMetricsUtil.dpToPx((int) margin);
+        mShotDetail.animate().translationY(translationValue)
                 .setInterpolator(new LinearOutSlowInInterpolator())
-                .setListener(mAnimatorListener).start();
+                .setDuration(700)
+                .setListener(mAnimatorListener)
+                .start();
     }
 
     private Animator.AnimatorListener mAnimatorListener = new Animator.AnimatorListener() {
